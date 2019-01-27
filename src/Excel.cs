2a4413bcfc467681a01b78ff4eede9ed679b5762
David Thole <david@thedarktrumpet.com>
2019-01-27 09:35:38 -0600

    Adding loading of the excel file

--- src/Excel.cs
+++ src/Excel.cs
@@ -1,17 +1,39 @@
 using ExcelDataReader;
+using System.Data;
+using System.IO;
 
 namespace Pick5Grammar.src
 {
-    public class Excel
+    public sealed class Excel
     {
         private readonly string _inFile;
         private readonly int _numOutput;
+        private DataTable _fullTable;
+
         public Excel(string inFile, int numOutput = 5)
         {
             _inFile = inFile;
             _numOutput = numOutput;
         }
 
-        
+        private void ReadExcelFile()
+        {
+            using (var stream = File.Open(@"\\tsclient\Desktop\Example.xlsx", FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+	            {
+		            ExcelDataSetConfiguration cofig = new ExcelDataSetConfiguration()
+		            {
+			            ConfigureDataTable = _ => new ExcelDataTableConfiguration() { UseHeaderRow = true}
+		            };
+
+		            DataSet result = reader.AsDataSet(cofig);
+                    _fullTable = result.Tables[0];
+	            }
+            }
+        }
+        public override string ToString() {
+            return "foo, for now";
+        }
     }
 }
