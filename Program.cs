ce5a42fba12ca031d98a956b28d64e2a1f37bfc5
David Thole <david@thedarktrumpet.com>
2019-01-27 11:56:01 -0600

    Adding command line nuget package and adding options

--- Program.cs
+++ Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using Pick5Grammar.src;
+using CommandLine;
+using System.Collections.Generic;
 
 namespace Pick5Grammar
 {
     class Program
     {
+        class Options
+        {
+            [Option('i', "input", Required = true, HelpText = "Input xlsx file")]
+            public string InputFile {get; set; }
+
+            [Option('c', "count", Required=false, HelpText="Number of random entries to receive")]
+            public int randomEntries {get; set; } = 5;
+        }
         static void Main(string[] args)
         {
-            Excel excel = new Excel("Example.xlsx", args.Length > 0 ? int.Parse(args[0]) : 5);
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(opts => ExecutePick5(opts));
+        }
+
+        static void ExecutePick5(Options options)
+        {
+            Excel excel = new Excel(options.InputFile, options.randomEntries);
         }
     }
 }
